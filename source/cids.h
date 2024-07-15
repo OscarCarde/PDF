86d93b490e57b502345a730faea0e5c9a990e14c
OscarCarde <68070683+OscarCarde@users.noreply.github.com>
2024-07-15 16:06:17 +0100

    first commit

---
+++ source/cids.h
@@ -0,0 +1,18 @@
+//------------------------------------------------------------------------
+// Copyright(c) 2024 Oscarde.
+//------------------------------------------------------------------------
+
+#pragma once
+
+#include "pluginterfaces/base/funknown.h"
+#include "pluginterfaces/vst/vsttypes.h"
+
+namespace MyCompanyName {
+//------------------------------------------------------------------------
+static const Steinberg::FUID kPDFProcessorUID (0x36A3C93E, 0xCA2651C0, 0x8C285B95, 0x0DE60886);
+static const Steinberg::FUID kPDFControllerUID (0xE561A2A9, 0x3A27562F, 0xA241C049, 0xD6BED5F3);
+
+#define PDFVST3Category "Fx"
+
+//------------------------------------------------------------------------
+} // namespace MyCompanyName
